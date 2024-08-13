22eb3ef24fba743f8588296abd999c98e2610aa2
RafaelLSartori <rafa_sartori55@hotmail.com>
2024-08-13 10:21:17 -0300

    Atualização

--- src/controller/Exercicio3.java
+++ src/controller/Exercicio3.java
@@ -1,2 +1,20 @@
-package controller;public class Exercicio3 {
+package controller;
+import java.util.Random;
+
+public class Exercicio3{
+    public Exercicio3(){
+        super();
+    }
+    Random random = new Random();
+    public void exTres(int[] vet){
+
+        for (int i: vet) {
+            int num = random.nextInt(100);
+            if (num % 2 != 0){
+                System.out.println("O número "+num+" é ímpar.");
+            } else if (num % 10 == 0){
+                System.out.println("O número "+num+" é par e múltiplo de 10.");
+            }
+        }
+    }
 }
--- src/view/Main.java
+++ src/view/Main.java
@@ -1,5 +1,18 @@
+package view;
+
+import controller.Exercicio3;
+
+import javax.swing.JOptionPane;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Exercicio3 m = new Exercicio3();
+        int tamanho = Integer.parseInt(JOptionPane.showInputDialog("Digite o tamanho do vetor: "));
+        while (tamanho > 100){
+            JOptionPane.showMessageDialog(null, "O vetor precisa ter no máximo 100 posições.");
+            tamanho = Integer.parseInt(JOptionPane.showInputDialog("Digite novamente o tamanho do vetor: "));
+        }
+        int[] vet = new int[tamanho];
+        m.exTres(vet);
     }
 }
